af5de5514e2302b7a0aa46aae160e86c46a42ded
Alamin-22 <mdalaminmollick@gmail.com>
2023-10-02 15:42:20 +0600

    private route setup is start

--- src/Provider/AuthProvider.jsx
+++ src/Provider/AuthProvider.jsx
@@ -1,5 +1,5 @@
-import { createContext } from "react";
-import { GoogleAuthProvider, signInWithPopup, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+import { createContext, useEffect, useState, } from "react";
+import { GoogleAuthProvider, signInWithPopup, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged, signOut, } from "firebase/auth";
 import auth from "../Firebase/Firebase.config";
 import PropTypes from 'prop-types'; // ES6
 
@@ -17,6 +17,8 @@ const GoogleProvider = new GoogleAuthProvider();
 
 const AuthProvider = ({ children }) => {
 
+    const [user, setUser] = useState({});
+
     // google login
     const handleGoogleLogin = () => {
         return signInWithPopup(auth, GoogleProvider)
@@ -31,17 +33,37 @@ const AuthProvider = ({ children }) => {
     const handleSingIn = (email, password) => {
         return signInWithEmailAndPassword(auth, email, password)
     }
+    // logout
+    const LogOut = () => {
+        return signOut(auth)
+    }
 
 
+    useEffect(() => {
+        onAuthStateChanged(auth, (user) => {
+            setUser(user);
+        })
+    }, [])
 
 
+    // useEffect(() => {
+    //    const unsubscribe= onAuthStateChanged(auth, (user) => {
+    //         setUser(user);
+    //     });
 
+    //     return ()=>{
+    //         unsubscribe()
+    //     }
+    // }, [])
 
+    console.log(user)
 
     const AuthValue = {
         handleGoogleLogin,
         handleEmailCreateUser,
         handleSingIn,
+        LogOut,
+        user,
     }
 
     return (
